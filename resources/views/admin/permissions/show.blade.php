a7c52bc19710b8c002f9096aec79f2c03cb514c2
lcloss <lcloss@gmail.com>
2021-09-20 01:56:19 +0100

    Component views

--- resources/views/admin/permissions/show.blade.php
+++ resources/views/admin/permissions/show.blade.php
@@ -1,19 +1,19 @@
 @extends('admin.layouts.app')
 @section('content')
     <x-views.show-view
-        title="Roles" title-singular="Role"
-        object="roles" object-singular="role"
-        :model="$role" :ref="$role->name">
+        title="Permissions" title-singular="Permission"
+        object="permissions" object-singular="permission"
+        :model="$permission" :ref="$permission->name">
 
         <div class="row">
-            <x-show-field colClass="col-md-6" label="Role">{{ $role->name ?? ' ' }}</x-show-field>
+            <x-show-field colClass="col-md-6" label="Permission">{{ $permission->name ?? ' ' }}</x-show-field>
             <x-show-field colClass="col-md-6" label="Roles">
-                @foreach( $role->permissions as $permission )
-                    <span class="badge badge-primary">{{ $permission->name ?? '' }}</span>
+                @foreach( $permission->roles as $role )
+                    <span class="badge badge-primary">{{ $role->name ?? '' }}</span>
                 @endforeach
             </x-show-field>
-            <x-show-field colClass="col-lg-2 col-md-3" label="Created at">{{ $role->created_at }}</x-show-field>
-            <x-show-field colClass="col-lg-2 col-md-3" label="Updated at">{{ $role->updated_at }}</x-show-field>
+            <x-show-field colClass="col-lg-2 col-md-3" label="Created at">{{ $permission->created_at }}</x-show-field>
+            <x-show-field colClass="col-lg-2 col-md-3" label="Updated at">{{ $permission->updated_at }}</x-show-field>
         </div>
 
     </x-views.show-view>
